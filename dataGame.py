26c74d9f7e17d58c75a246a4cb8f598e262b8abf
the.tranthanh <phonvan128@gmail.com>
2022-12-01 23:52:26 +0700

    push code

---
+++ dataGame.py
@@ -0,0 +1,188 @@
+
+def mathData():
+  return [
+    {
+      'question': '3 + 4 = ?',
+      'answer': 'seven'
+    },
+    {
+      'question': '5 + 4 = ?',
+      'answer': 'nine'
+    },
+    {
+      'question': '3 + 4 = ?',
+      'answer': 'seven'
+    },
+    {
+      'question': '5 + 4 = ?',
+      'answer': 'nine'
+    },
+    {
+      'question': '1 + 1 = ?',
+      'answer': 'two'
+    },
+    {
+      'question': '0 + 4 = ?',
+      'answer': 'four'
+    },
+    {
+      'question': '3 + 4 = ?',
+      'answer': 'seven'
+    },
+    {
+      'question': '5 + 5 = ?',
+      'answer': 'ten'
+    },
+    {
+      'question': '10 + 10 = ?',
+      'answer': 'twenty'
+    },
+    {
+      'question': '5 + 1 = ?',
+      'answer': 'six'
+    },
+    {
+      'question': '1 + 4 = ?',
+      'answer': 'five'
+    },
+    {
+      'question': '4 + 4 = ?',
+      'answer': 'eight'
+    },
+    {
+      'question': '7 + 4 = ?',
+      'answer': 'eleven'
+    },
+    {
+      'question': '5 + 12 = ?',
+      'answer': 'seventeen'
+    },{
+      'question': '3 + 13 = ?',
+      'answer': 'sixteen'
+    },
+    {
+      'question': '9 + 4 = ?',
+      'answer': 'thirteen'
+    },{
+      'question': '3 + 10 = ?',
+      'answer': 'thirteen'
+    },
+    {
+      'question': '5 + 8 = ?',
+      'answer': 'thirteen'
+    },{
+      'question': '3 + 14 = ?',
+      'answer': 'seventeen'
+    },
+    {
+      'question': '5 + 4 = ?',
+      'answer': 'nine'
+    },{
+      'question': '8 + 9 = ?',
+      'answer': 'seventeen'
+    },
+    {
+      'question': '5 + 6 = ?',
+      'answer': 'eleven'
+    },{
+      'question': '3 + 8 = ?',
+      'answer': 'eleven'
+    },
+    {
+      'question': '1 + 10 = ?',
+      'answer': 'eleven'
+    },{
+      'question': '6 + 4 = ?',
+      'answer': 'ten'
+    },
+    {
+      'question': '5 + 5 = ?',
+      'answer': 'ten'
+    },{
+      'question': '3 + 7 = ?',
+      'answer': 'ten'
+    },
+    {
+      'question': '1 + 9 = ?',
+      'answer': 'ten'
+    },
+  ]
+
+def grammarData():
+  return [
+    {
+      'question': 'it is(rain)......',
+      'answer': 'raining'
+    },
+    {
+      'question': 'she(go)...... to school',
+      'answer': 'goes'
+    },
+    {
+      'question': 'the boy(be)..... eating cake',
+      'answer': 'is'
+    },
+    {
+      'question': 'she(walk)....... to work',
+      'answer': 'walks'
+    },
+    {
+      'question': 'I am(do)...... my homework',
+      'answer': 'doing'
+    },
+    {
+      'question': 'i(be)...... a student',
+      'answer': 'am'
+    },
+    {
+      'question': 'I(go)...... out yesterday',
+      'answer': 'went'
+    },
+    {
+      'question': 'I will(buy)...... a book tomorrow',
+      'answer': 'buy'
+    },
+    {
+      'question': 'I(play)..... basketball last week',
+      'answer': 'played'
+    }
+  ]
+
+def guessPictureData():
+  return [
+    {
+      'question': 'What is it?',
+      'answer': 'apple',
+      'url_image': 'images\\apple.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'ears',
+      'url_image': 'images\\ears.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'egg',
+      'url_image': 'images\\egg.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'elephant',
+      'url_image': 'images\\elephant.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'flower',
+      'url_image': 'images\\flower.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'sheep',
+      'url_image': 'images\\sheep.png'
+    },
+    {
+      'question': 'What is it?',
+      'answer': 'table',
+      'url_image': 'images\\table.png'
+    },
+  ]
